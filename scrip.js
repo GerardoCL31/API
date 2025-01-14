720d664eedb5fd7ce2e772ba8a18e226243ff30c
GerardoCL31 <gerardotrajano@gmail.com>
2025-01-14 14:00:01 +0100

    css y js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,5 @@
-const form = document.getElementById
+const form = document.getElementById('pokemon-form');
+const input = document.getElementById('pokemon-name');
+const pokemponINfoDiv = document.getElementById('pokemon-info');
+
+async function fecthPokemonData 
