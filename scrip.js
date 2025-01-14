62cbfbe0d731a52ac208a00327177da62b03cd9b
GerardoCL31 <gerardotrajano@gmail.com>
2025-01-14 14:25:41 +0100

    js

--- scrip.js
+++ scrip.js
@@ -2,4 +2,28 @@ const form = document.getElementById('pokemon-form');
 const input = document.getElementById('pokemon-name');
 const pokemponINfoDiv = document.getElementById('pokemon-info');
 
-async function fecthPokemonData 
+async function fecthPokemonData(pokemonName){
+    try{
+        const reponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+
+        if (!reponse.ok){
+            throw new error('Pokémon no encontrado');
+        }
+            const data = await reponse.json();
+            displayPokemonInfo(data);
+        } catch (error) {
+            pokemponINfoDiv.innerHTML = `<p>${error.message}</p>`;
+            pokemponINfoDiv.style.display = 'block';
+        }
+        function displayPokemonInfo(pokemon){
+            const{name,sprites,types,abilities} = pokemon;
+            pokemponINfoDiv.innerHTML = `
+            <img src="${sprites.front_default} alt="${name}">
+            <h3>${name}</h3>
+            <p>Tipo:${types}</p>
+            <p>habilidades:${abilities}</p>
+            `;
+            pokemponINfoDiv.style.display = 'block';
+        }
+    }
+    
