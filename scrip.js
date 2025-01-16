72e9693508564698637f326a85aef142c83b68bf
GerardoCL31 <gerardotrajano@gmail.com>
2025-01-16 12:42:21 +0100

    js debe de duncionar

--- scrip.js
+++ scrip.js
@@ -1,36 +1,40 @@
 const form = document.getElementById('pokemon-form');
 const input = document.getElementById('pokemon-name');
-const pokemponINfoDiv = document.getElementById('pokemon-info');
+const pokemonInfoDiv = document.getElementById('pokemon-info');
 
-async function fecthPokemonData(pokemonName){
-    try{
-        const reponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+async function fetchPokemonData(pokemonName) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+        
+        if (!response.ok) {
+            throw new Error('Pokémon no encontrado');
+        }
 
-        if (!reponse.ok){
-            throw new error('Pokémon no encontrado');
-        }
-            const data = await reponse.json();
-            displayPokemonInfo(data);
-        } catch (error) {
-            pokemponINfoDiv.innerHTML = `<p>${error.message}</p>`;
-            pokemponINfoDiv.style.display = 'block';
-        }
-        function displayPokemonInfo(pokemon){
-            const{name,sprites,types,abilities} = pokemon;
-            pokemponINfoDiv.innerHTML = `
-            <img src="${sprites.front_default} alt="${name}">
-            <h3>${name}</h3>
-            <p>Tipo:${types}</p>
-            <p>habilidades:${abilities}</p>
-            `;
-            pokemponINfoDiv.style.display = 'block';
-        }
-        form.addEventListener('submit', function(event){
-            event.preventDefault();
-            const pokemonName = input.ariaValueMax.trim();
-            if (pokemon){
-                fecthPokemonData(pokemonName);
-            }
-        });
+        const data = await response.json()
+        displayPokemonInfo(data);
+    } catch (error) {
+            pokemonInfoDiv.innerHTML = `<p>${error.message}</p>`;
+            pokemonInfoDiv.style.display = 'block';
     }
-    
+
+    function displayPokemonInfo(pokemon) {
+        const { name, sprites, types, abilities } = pokemon;
+
+        pokemonInfoDiv.innerHTML = `
+        <img src="${sprites.front_default}" alt="${name}">
+        <h3>${name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+        <p><strong>Tipo:</strong>${types.map(type => type.type.name).join(', ')}</p>
+        <p><strong>Habilidades:</strong>${abilities.map(ability => ability.ability.name).join(', ')}</p>
+        `;
+
+        pokemonInfoDiv.style.display = 'block';
+    }
+}
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const pokemonName = input.value.trim();
+        if (pokemonName) {
+            fetchPokemonData(pokemonName);
+        }
+    });
